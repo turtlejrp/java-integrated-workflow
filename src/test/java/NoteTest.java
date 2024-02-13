fc2ec2eda5a8dc5ff2fc0b0a925efbe360068457
Turtlejrp <jirapass78@gmail.com>
2024-02-13 11:52:59 +0700

    Add import assetThrow

--- src/test/java/NoteTest.java
+++ src/test/java/NoteTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
